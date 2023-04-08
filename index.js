6bc8f4392e4e4ca4dabd78c3d9355b0a08b7e7ec
Zheng Fu <24203166+fuzheng1998@users.noreply.github.com>
2023-04-09 09:12:57 +1000

    update js func

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // a function set element text content as data in json format
 function setElementTextContent(element, data) {
-    element.textContent = JSON.stringify(data, null, 2);
+    element.textContent = data
 }
