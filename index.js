77e3e66a5dc646e57903280cd68860b4573dbb87
Zheng Fu <24203166+fuzheng1998@users.noreply.github.com>
2023-05-05 10:30:25 +1000

    rebuild project using bootstrap

--- index.js
+++ index.js
@@ -1,4 +1,49 @@
-// a function set element text content as data in json format
-function setElementTextContent(element, data) {
-    element.textContent = data
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggle = document.getElementById('themeToggle');
+    const navLinks = document.querySelectorAll('.nav-link');
+
+    themeToggle.addEventListener('click', () => {
+        document.body.classList.toggle('dark-theme');
+    });
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const tab = link.getAttribute('data-tab');
+            setActiveTab(tab);
+        });
+    });
+
+    function setActiveTab(tab) {
+        navLinks.forEach(link => {
+            link.classList.toggle('active', link.getAttribute('data-tab') === tab);
+        });
+
+        const tabPanes = document.querySelectorAll('.tab-pane');
+        tabPanes.forEach(pane => {
+            pane.classList.toggle('active', pane.getAttribute('id') === tab);
+        });
+    }
+});
+function animateLoadingBar() {
+    const loadingBar = document.getElementById('loadingBar');
+    let width = 0;
+    const interval = setInterval(frame, 50); // Update every 50ms
+
+    function frame() {
+        if (width >= 100) {
+            clearInterval(interval);
+            // Hide loading overlay and display page content when the progress bar reaches 100%
+            document.getElementById('loadingOverlay').style.display = 'none';
+            document.getElementById('pageContent').style.display = 'block';
+        } else {
+            width++;
+            loadingBar.style.width = width + '%';
+            loadingBar.setAttribute('aria-valuenow', width);
+            loadingBar.textContent = width + '%';
+        }
+    }
 }
+document.addEventListener('DOMContentLoaded', function() {
+    animateLoadingBar();
+});
